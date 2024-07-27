0552cfa89586947fd059cfa14346b45b5397f8b1
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-07-27 20:30:29 +0300

    Update W_H.py

--- W_H.py
+++ W_H.py
@@ -206,8 +206,8 @@ def display_batch_details_and_confirmation():
         
     else:
         st.error("العمود 'Batch No' غير موجود في البيانات.")
-    else:
-        st.error(f"الدفعة {batch_number} غير موجودة!")
+        else:
+            st.error(f"الدفعة {batch_number} غير موجودة!")
 
 
 
