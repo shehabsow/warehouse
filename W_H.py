5dcf5f3786703b3692e5d1d3e09d621ed72b8760
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-07-27 20:31:19 +0300

    Update W_H.py

--- W_H.py
+++ W_H.py
@@ -197,8 +197,7 @@ def display_batch_details_and_confirmation():
     
     if st.button("عرض الدفعة"):
         batch_df = df_Receving1[df_Receving1['Batch No'] == batch_number]
-        if not batch_df.empty:
-            st.dataframe(batch_df)
+        st.dataframe(batch_df)
 
     if st.button("تأكيد الدفعة"):
         st.dataframe(batch_df)
@@ -206,8 +205,8 @@ def display_batch_details_and_confirmation():
         
     else:
         st.error("العمود 'Batch No' غير موجود في البيانات.")
-        else:
-            st.error(f"الدفعة {batch_number} غير موجودة!")
+    else:
+        st.error(f"الدفعة {batch_number} غير موجودة!")
 
 
 
