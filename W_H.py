26695c7dfd6b15c220b87f7b1928759f1d61485e
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-08-06 17:21:34 +0300

    Update W_H.py

--- W_H.py
+++ W_H.py
@@ -426,7 +426,7 @@ else:
                     search_keyword = st.session_state.get('search_keyword', '')
                     search_keyword = st.text_input("Enter keyword to search:", search_keyword)
                     search_button = st.button("Search")
-                     earch_option = 'All Columns'
+                    search_option  = 'All Columns'
                 
                 def search_in_dataframe(df_f, keyword, option):
                     if option == 'All Columns':
