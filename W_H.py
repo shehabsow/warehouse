015cc4ad477e68155e0afbb66c82228b16ade8b5
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-08-08 16:45:42 +0300

    Update W_H.py

--- W_H.py
+++ W_H.py
@@ -238,7 +238,7 @@ def display_batch_details_and_confirmation():
             st.error("Batch file is not available.")
             return
 
-        col1, col2 = st.columns([2, 0.75])
+        col1, col2 = st.columns([1, .5])
         with col1:
 
             batch_number = st.selectbox("Select a batch number", df_Receving['Batch No'].unique())
